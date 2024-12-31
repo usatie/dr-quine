badf32270d5fe9ad140867aed1308ff19a6ab3c0
Shun Usami <usatie@gmail.com>
2025-01-01 01:24:37 +0900

    Refactor quine_exec_v2.c to use a single QUINE_PRINT macro

--- quine_exec_v2.c
+++ quine_exec_v2.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#define QUINE_PRINT(code, i) dprintf(fd, code, i - 1, #code)
+int i = 5;
+#define QUINE_PRINT(code) dprintf(fd, code, i - 1, #code)
 
 int main() {
-    int i = 5;
     if (i < 0) return -1;
     char filename[100], exec_path[100], command[100];
     sprintf(filename, "quine_exec_%d.c", i);
@@ -17,22 +17,21 @@ int main() {
         "#include <unistd.h>\n"
         "#include <fcntl.h>\n"
         "#include <stdio.h>\n"
-        "#define QUINE_PRINT(code, i) dprintf(fd, code, i - 1, #code)\n"
+        "int i = %d;\n"
+        "#define QUINE_PRINT(code) dprintf(fd, code, i - 1, #code)\n"
         "int main() {\n"
-        "    int i = %d;\n"
 		"    if (i < 0) return -1;\n"
 		"    char filename[100], exec_path[100], command[100];\n"
 		"    sprintf(filename, \"quine_exec_%%d.c\", i);\n"
 		"    sprintf(exec_path, \"quine_exec_%%d\", i);\n"
 		"    sprintf(command, \"clang -Wall -Wextra -Werror %%s -o %%s\", filename, exec_path);\n"
         "    int fd = open(filename, O_CREAT | O_WRONLY, 0644);\n"
-        "    QUINE_PRINT(%s, i);\n"
+        "    QUINE_PRINT(%s);\n"
         "    close(fd);\n"
         "    system(command);\n"
 		"    execv(exec_path, NULL);\n"
         "    return 0;\n"
-        "}\n"
-    , i);
+        "}\n");
     close(fd);
     system(command);
     execv(exec_path, NULL);
