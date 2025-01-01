bc1ae7f582e5013c58969866795834d2276beeb7
Shun Usami <usatie@gmail.com>
2025-01-01 21:45:55 +0900

    Refactor the quine macro to be more readable and use three define and comment

--- quine_macro.c
+++ quine_macro.c
@@ -1,2 +1,10 @@
-#define Q(x) int main() { char *s = #x; __builtin_printf(x, s); }
-Q("#define Q(x) int main() { char *s = #x; __builtin_printf(x, s); }\nQ(%s)\n")
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+/*
+    This program will write its own source code to file named "Grace_kid.c" when run.
+*/
+#define FT(x) int main() { int fd = open("Grace_kid.c", O_CREAT | O_WRONLY, 0644); dprintf(fd, x, REPR(x)); close(fd); }
+#define SELF "#include <stdio.h>\n#include <fcntl.h>\n#include <unistd.h>\n/*\n    This program will write its own source code to file named \"Grace_kid.c\" when run.\n*/\n#define FT(x) int main() { int fd = open(\"Grace_kid.c\", O_CREAT | O_WRONLY, 0644); dprintf(fd, x, REPR(x)); close(fd); }\n#define SELF %s\n#define REPR(x) #x\nFT(SELF)\n"
+#define REPR(x) #x
+FT(SELF)
