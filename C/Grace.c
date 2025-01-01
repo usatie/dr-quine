42270ddf6798e52de44740bfa1aeaad73fcde3ca
Shun Usami <usatie@gmail.com>
2025-01-02 00:16:37 +0900

    Refactor Grace.c include order

--- C/Grace.c
+++ C/Grace.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 /*
     This program will write its own source code to file named "Grace_kid.c" when run.
 */
 #define FT(x) int main() { int fd = open("Grace_kid.c", O_CREAT | O_WRONLY, 0644); dprintf(fd, x, REPR(x)); close(fd); }
-#define SELF "#include <stdio.h>\n#include <fcntl.h>\n#include <unistd.h>\n/*\n    This program will write its own source code to file named \"Grace_kid.c\" when run.\n*/\n#define FT(x) int main() { int fd = open(\"Grace_kid.c\", O_CREAT | O_WRONLY, 0644); dprintf(fd, x, REPR(x)); close(fd); }\n#define SELF %s\n#define REPR(x) #x\nFT(SELF)\n"
+#define SELF "#include <fcntl.h>\n#include <stdio.h>\n#include <unistd.h>\n/*\n    This program will write its own source code to file named \"Grace_kid.c\" when run.\n*/\n#define FT(x) int main() { int fd = open(\"Grace_kid.c\", O_CREAT | O_WRONLY, 0644); dprintf(fd, x, REPR(x)); close(fd); }\n#define SELF %s\n#define REPR(x) #x\nFT(SELF)\n"
 #define REPR(x) #x
 FT(SELF)
