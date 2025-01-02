48a958302d545c014eb80fdab4c9674bbe27ae6a
Shun Usami <usatie@gmail.com>
2025-01-02 23:53:52 +0900

    Fix Grace to write to a file and close it

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -3,10 +3,11 @@
 %define DOUBLE_QUOTE 34
 %macro MAIN 0
 global  main
-extern  printf
+extern  dprintf
 
-section .data
-    src   db  "; This program will print its own source when run.%1$c%%define NEWLINE 10%1$c%%define DOUBLE_QUOTE 34%1$c%%macro MAIN 0%1$cglobal  main%1$cextern  printf%1$c%1$csection .data%1$c    src   db  %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c    push rbp%1$c    mov rbp, rsp%1$c    pop rbp%1$c    ret%1$cmain:%1$c    push    rbp         ;%1$c    mov     rbp, rsp    ;%1$c%1$c    call foo%1$c%1$c    lea     rdi, [rel src]%1$c    mov     rsi, NEWLINE%1$c    mov     rdx, DOUBLE_QUOTE%1$c    lea     rcx, [rel src]%1$c    xor     eax, eax%1$c    call    printf wrt ..plt%1$c%1$c    mov     eax, 0%1$c%1$c    leave%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%%endmacro%1$c%1$cMAIN%1$c", 0
+section .rodata
+    src   db  "; This program will print its own source when run.%1$c%%define NEWLINE 10%1$c%%define DOUBLE_QUOTE 34%1$c%%macro MAIN 0%1$cglobal  main%1$cextern  dprintf%1$c%1$csection .rodata%1$c    src   db  %2$c%3$s%2$c, 0%1$c    file  db  %2$cGrace_kid.s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c    push rbp%1$c    mov rbp, rsp%1$c    pop rbp%1$c    ret%1$cmain:%1$c    push    rbp%1$c    mov     rbp, rsp%1$c    %1$c    mov rdi, -100%1$c    lea rsi, [rel file]%1$c    mov rdx, (0o100|0o01|0o1000)%1$c    mov r10, 0o644%1$c    mov rax, 0x101%1$c    syscall%1$c    %1$c    push    rax%1$c    %1$c    mov     rdi, rax%1$c    lea     rsi, [rel src]%1$c    mov     rdx, NEWLINE%1$c    mov     rcx, DOUBLE_QUOTE%1$c    lea     r8, [rel src]%1$c    xor     eax, eax%1$c    call    dprintf wrt ..plt%1$c    %1$c    pop     rdi%1$c    mov     rax, 0x3	; syscall number for close%1$c    syscall%1$c    %1$c    mov     eax, 0%1$c    %1$c    leave%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%%endmacro%1$c%1$cMAIN%1$c", 0
+    file  db  "Grace_kid.s", 0
 
 section .text
 foo:
@@ -15,20 +16,32 @@ foo:
     pop rbp
     ret
 main:
-    push    rbp         ;
-    mov     rbp, rsp    ;
-
-    call foo
-
-    lea     rdi, [rel src]
-    mov     rsi, NEWLINE
-    mov     rdx, DOUBLE_QUOTE
-    lea     rcx, [rel src]
+    push    rbp
+    mov     rbp, rsp
+    
+    mov rdi, -100
+    lea rsi, [rel file]
+    mov rdx, (0o100|0o01|0o1000)
+    mov r10, 0o644
+    mov rax, 0x101
+    syscall
+    
+    push    rax
+    
+    mov     rdi, rax
+    lea     rsi, [rel src]
+    mov     rdx, NEWLINE
+    mov     rcx, DOUBLE_QUOTE
+    lea     r8, [rel src]
     xor     eax, eax
-    call    printf wrt ..plt
-
+    call    dprintf wrt ..plt
+    
+    pop     rdi
+    mov     rax, 0x3	; syscall number for close
+    syscall
+    
     mov     eax, 0
-
+    
     leave
     ret
 
