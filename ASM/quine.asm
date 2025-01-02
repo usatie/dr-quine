c6d44922a792358b79dd6977af13f07d54820c0c
Shun Usami <usatie@gmail.com>
2025-01-02 17:34:34 +0900

    Implement quine in ASM

--- ASM/quine.asm
+++ ASM/quine.asm
@@ -1,19 +1,33 @@
+; This program will print its own source when run.
+global  main
+extern  printf
+
 section .data
-    msg: db "Hello World!", 10
-    msgSize equ $ - msg
+    src   db  "; This program will print its own source when run.%1$cglobal  main%1$cextern  printf%1$c%1$csection .data%1$c    src   db  %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c    push rbp%1$c    mov rbp, rsp%1$c    pop rbp%1$c    ret%1$cmain:%1$c    push    rbp         ;%1$c    mov     rbp, rsp    ;%1$c%1$c	call foo%1$c%1$c    ; printf(%2$cHello, world!%2$c);%1$c    lea     rdi, [rel src]%1$c	mov     rsi, 10%1$c	mov     rdx, 34%1$c	lea     rcx, [rel src]%1$c    xor     eax, eax%1$c    call    printf wrt ..plt%1$c%1$c    mov     eax, 0%1$c%1$c    leave%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c", 0
 
 section .text
-global _start
+foo:
+    push rbp
+    mov rbp, rsp
+    pop rbp
+    ret
+main:
+    push    rbp         ;
+    mov     rbp, rsp    ;
 
-_start:
-    ;; sys_write(STDOUT=1, msg, msgSize)
-    mov rax, 1           ; syscall number: sys_write
-    mov rdi, 1           ; file descriptor: stdout
-    mov rsi, msg         ; address of the message
-    mov rdx, msgSize     ; length
-    syscall
+	call foo
 
-    ;; sys_exit(0)
-    mov rax, 60          ; syscall number: sys_exit
-    xor rdi, rdi         ; exit code 0
-    syscall
+    ; printf("Hello, world!");
+    lea     rdi, [rel src]
+	mov     rsi, 10
+	mov     rdx, 34
+	lea     rcx, [rel src]
+    xor     eax, eax
+    call    printf wrt ..plt
+
+    mov     eax, 0
+
+    leave
+    ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
