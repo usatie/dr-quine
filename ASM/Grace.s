5534aaa1c35e69ba5e34442f1857613230441fb8
Shun Usami <usatie@gmail.com>
2025-01-03 07:28:18 +0900

    Refactor Grace.s to use a stack frame and local variables.
    - Add comments to the code
    to explain the purpose of each section.

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -6,18 +6,19 @@ global  main
 extern  dprintf
 
 section .rodata
-    src   db  "; This program will print its own source when run.%1$c%%define NEWLINE 10%1$c%%define DOUBLE_QUOTE 34%1$c%%macro MAIN 0%1$cglobal  main%1$cextern  dprintf%1$c%1$csection .rodata%1$c    src   db  %2$c%3$s%2$c, 0%1$c    file  db  %2$cGrace_kid.s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c    push rbp%1$c    mov rbp, rsp%1$c    pop rbp%1$c    ret%1$cmain:%1$c    push    rbp%1$c    mov     rbp, rsp%1$c    %1$c    mov rdi, -100%1$c    lea rsi, [rel file]%1$c    mov rdx, (0o100|0o01|0o1000)%1$c    mov r10, 0o644%1$c    mov rax, 0x101%1$c    syscall%1$c    %1$c    push    rax%1$c    %1$c    mov     rdi, rax%1$c    lea     rsi, [rel src]%1$c    mov     rdx, NEWLINE%1$c    mov     rcx, DOUBLE_QUOTE%1$c    lea     r8, [rel src]%1$c    xor     eax, eax%1$c    call    dprintf wrt ..plt%1$c    %1$c    pop     rdi%1$c    mov     rax, 0x3	; syscall number for close%1$c    syscall%1$c    %1$c    mov     eax, 0%1$c    %1$c    leave%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%%endmacro%1$c%1$cMAIN%1$c", 0
+    src   db  "; This program will print its own source when run.%1$c%%define NEWLINE 10%1$c%%define DOUBLE_QUOTE 34%1$c%%macro MAIN 0%1$cglobal  main%1$cextern  dprintf%1$c%1$csection .rodata%1$c    src   db  %2$c%3$s%2$c, 0%1$c    file  db  %2$cGrace_kid.s%2$c, 0%1$c%1$csection .text%1$cfoo:%1$c    push rbp%1$c    mov rbp, rsp%1$c    leave%1$c    ret%1$cmain:%1$c    push    rbp%1$c    mov     rbp, rsp%1$c    sub     rsp, 0x10%1$c    %1$c    mov rdi, -100%1$c    lea rsi, [rel file]%1$c    mov rdx, (0o100|0o01|0o1000)%1$c    mov r10, 0o644%1$c    mov rax, 0x101%1$c    syscall%1$c    mov     [rbp-0x8], rax%1$c    %1$c    mov     rdi, rax%1$c    lea     rsi, [rel src]%1$c    mov     rdx, NEWLINE%1$c    mov     rcx, DOUBLE_QUOTE%1$c    lea     r8, [rel src]%1$c    xor     eax, eax%1$c    call    dprintf wrt ..plt%1$c    %1$c    mov     rdi, [rbp-0x8]%1$c    mov     rax, 0x3%1$c    syscall%1$c    %1$c    mov     eax, 0%1$c    %1$c    leave%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%%endmacro%1$c%1$cMAIN%1$c", 0
     file  db  "Grace_kid.s", 0
 
 section .text
 foo:
     push rbp
     mov rbp, rsp
-    pop rbp
+    leave
     ret
 main:
     push    rbp
     mov     rbp, rsp
+    sub     rsp, 0x10
     
     mov rdi, -100
     lea rsi, [rel file]
@@ -25,8 +26,7 @@ main:
     mov r10, 0o644
     mov rax, 0x101
     syscall
-    
-    push    rax
+    mov     [rbp-0x8], rax
     
     mov     rdi, rax
     lea     rsi, [rel src]
@@ -36,8 +36,8 @@ main:
     xor     eax, eax
     call    dprintf wrt ..plt
     
-    pop     rdi
-    mov     rax, 0x3	; syscall number for close
+    mov     rdi, [rbp-0x8]
+    mov     rax, 0x3
     syscall
     
     mov     eax, 0
---
+++ ASM/Grace_with_comments.s
@@ -0,0 +1,60 @@
+; This program will print its own source when run.
+%define NEWLINE 10
+%define DOUBLE_QUOTE 34
+%macro MAIN 0
+global  main
+extern  dprintf
+
+section .rodata
+    src   db  "<source code>", 0
+    file  db  "Grace_kid.s", 0
+
+section .text
+foo:
+	; Function prologue
+    push rbp
+    mov rbp, rsp
+	; Function epilogue
+    pop rbp
+    ret
+main:
+	; Function prologue
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 0x10            ; Allocate space for local variables
+    
+	; int fd = open_at(AT_FDCWD, file, O_WRONLY|O_CREAT|O_TRUNC, 0644)
+    mov rdi, -100                ; AT_FDCWD
+    lea rsi, [rel file]          ; use relative addressing for PIC
+    mov rdx, (0o100|0o01|0o1000) ; O_WRONLY|O_CREAT|O_TRUNC
+    mov r10, 0o644			     ; 0644
+    mov rax, 0x101 			     ; syscall number for openat
+    syscall
+    mov     [rbp-0x8], rax
+    
+	; dprintf(fd, src, NEWLINE, DOUBLE_QUOTE, src)
+    mov     rdi, rax
+    lea     rsi, [rel src]
+    mov     rdx, NEWLINE
+    mov     rcx, DOUBLE_QUOTE
+    lea     r8, [rel src]
+    xor     eax, eax
+    call    dprintf wrt ..plt    ; wrt ..plt is needed for PIC
+    
+	; close(fd)
+    mov     rdi, [rbp-0x8]       ; Restore the file descriptor
+    mov     rax, 0x3	         ; syscall number for close
+    syscall
+    
+	; return 0
+    mov     eax, 0
+    
+	; Function epilogue
+    leave                        ; restore the stack frame
+    ret
+
+; This section is needed to prevent the program from being placed on the stack and suppresses the warning
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endmacro
+
+MAIN
